5b888b983a24fd499d89af8f0d27193f84de76b2
Mohit Lahoti <engtechno25@gmail.com>
2024-10-03 23:58:48 +0530

    Creating struct for hashed value and store the list into them

--- Elixir/identicon/lib/identicon.ex
+++ Elixir/identicon/lib/identicon.ex
@@ -2,12 +2,15 @@ defmodule Identicon do
 
   def main(input) do
     input
-    |> has_input
+    |> hash_input
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+
   end
 
 end
