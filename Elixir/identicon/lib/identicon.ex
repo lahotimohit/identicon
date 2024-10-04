5c168b4850b5e5a66dc55850c8ac3718adcba9bc
Mohit Lahoti <engtechno25@gmail.com>
2024-10-04 14:31:16 +0530

    Building the grid from hash string

--- Elixir/identicon/lib/identicon.ex
+++ Elixir/identicon/lib/identicon.ex
@@ -4,11 +4,16 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b |_tail]} = image
-    [r, g, b]
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
